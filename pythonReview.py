bb11fd668452d23e47ff875a0dd88d7523f285e6
galinasu23-meet <109904723+galinasu23-meet@users.noreply.github.com>
2022-07-25 19:03:30 +0300

    i did the bonus

--- pythonReview.py
+++ pythonReview.py
@@ -1,7 +1,7 @@
 
 
-def create_youtube_video(title,description):
-	video = {"title":title, "description":description, "likes":0, "dislikes": 0, "comments": {}}
+def create_youtube_video(title, description, hashtag):
+	video = {"title":title, "description":description, "likes":0, "dislikes": 0, "comments": {}, "hashtag":hashtag }
 	return video
 
 def like(vid):
@@ -18,12 +18,28 @@ def add_comment(youtubevideo, username, comment_text):
 	youtubevideo["comments"][username]= "comment_text"
 	return youtubevideo
 
-newvideo = create_youtube_video("Guys I Just Got Kidnapped!!!", "PLS SEND HELP")
+newvideo = create_youtube_video("Guys I Just Got Kidnapped!!!", "PLS SEND HELP",["#kidnapping", "sad", "#help", "#sendhelp", "#viral" ])
 add_comment(newvideo,"gali","same broooo")
 for i in range(495):
 	like(newvideo)
 
-print (newvideo["likes"])
+newvideo2 = create_youtube_video("kidnapping tutorial!!", "learn the best ways to Kidnap",["#kidnapping", "happy", "#tips", "#sendlove", "#viral" ])
+add_comment(newvideo,"gali","same broooo")
+for i in range(630):
+	like(newvideo2)
+
+def similarity_to_video (a,b):
+	counter = 0
+	for i in a:
+		if a[i]==b[i]:
+			counter +=1
+	precent = counter/len(b)
+	print ("the precent of similarity of", a["title"], "and",  b["title"], "is", precent* 100.0)
+similarity_to_video (newvideo,newvideo2)
+
+
+
+print (newvideo["likes"],newvideo2["likes"])
 
 
 
